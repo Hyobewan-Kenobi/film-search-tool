ad86d6be5e6566e8dd1c26a8edc8814be9ddf980
Hyo Kahng <hb1@hb1knb.mynetworksettings.com>
2024-09-06 18:01:50 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
+"use client"
+import { useState } from 'react';
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [id, setId] = useState('');
+  const [movieData, setMovieData] = useState(null);
+  const [error, setError] = useState(null)
+  const [isImageError, setIsImageError] = useState<boolean>(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const fetchMovie = async ()=>{
+    const apiKey = process.env.NEXT_PUBLIC_OMDB_API_KEY
+    const url = `http://www.omdbapi.com/?t=${id}&plot=full&apikey=${apiKey}`
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok){
+        throw new Error(`Error: ${response.status}`)
+      }
+
+      const data = await response.json();
+      setMovieData(data)
+      setError(null)
+    } catch (error: any) {
+      setError(error.message);
+      setMovieData(null)
+    }
+
+  }
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      fetchMovie();
+    }
+  };
+
+  const isPosterValid = (poster: string) => poster !== 'N/A';
+  
+  return (
+    <div>
+      <h1 className='font-bold text-4xl pt-4 flex flex-col justify-center items-center'>🎥 Film Search</h1>
+      <div className='flex flex-col justify-center items-center border-2 border-blue-500 max-w-[400px] mx-auto mt-4 p-6 rounded-xl mb-4'>
+        <p className="pb-4 text-center">Look up a movie or TV series, powered by OMDb API</p>
+        <div className='flex gap-2'>
+          <input
+            type="text"
+            placeholder="Enter Title of Film"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            onKeyDown={handleKeyPress}
+            className="rounded-full border-0 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+          />
+          <button className="rounded-full bg-blue-500 text-white font-bold px-4 py-2 text-l hover:bg-blue-700" onClick={fetchMovie}>Get Movie Info</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        
+
+        {error && <p className='pt-6' style={{ color: 'red' }}>{error}</p>}
+        {movieData && (
+          <div>
+            {isPosterValid(movieData.Poster) && !isImageError ? (
+              <Image
+                src={movieData.Poster}
+                width={200}
+                height={300}
+                alt="film poster"
+                className="py-4 mx-auto"
+                onError={() => setIsImageError(true)} // Handle broken images
+              />
+            ) : (
+              <p className='py-4 text-red-500'>Image not available</p>
+            )}
+            <div className='flex flex-col gap-2'>
+              <p>Title: {movieData.Title}</p>
+              <p>Released Date: {movieData.Released}</p>
+              <p>Type: {movieData.Type}</p>
+              <p>Country: {movieData.Country}</p>
+              <p>Genre: {movieData.Genre}</p>
+              <p>Director: {movieData.Director}</p>
+              <p>Writer: {movieData.Writer}</p>
+              <p>Actors: {movieData.Actors}</p>
+              <p>{movieData.Plot}</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  );
+  )
 }
